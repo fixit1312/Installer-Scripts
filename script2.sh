2807d63dd42df8f6aa237494a2bc025edfc53d5d
fixit1312 <fixit1411@gmail.com>
2024-08-23 21:12:30 +0300

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,5 @@
+sudo systemctl status proxyinstall.service
+sudo systemctl status vpsmanager.service
+sudo systemctl status pdfmaker.service
+sudo systemctl status mmmfilter.service
+service openvpn status
